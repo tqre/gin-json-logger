2b6be4d41e23cfd59483a13ecc76497c61576d9e
tqre <tkuure@gmail.com>
2020-06-12 20:01:44 +0300

    If TLS is used, version and used cipher are logged

--- logger_json.go
+++ logger_json.go
@@ -13,23 +13,28 @@ import (
 )
 
 type LogItems struct {
-	ISOTime       time.Time
-	UnixTime      int64
-	IP            string
-	Method        string
-	Path          string
-	Query         string
-	User          string
-	Protocol      string
-	ContentType   string
-	ContentLength int64
-	Host          string
-	// TLSVersion uint16
+	ISOTime        time.Time
+	UnixTime       int64
+	IP             string
+	Method         string
+	Path           string
+	Query          string
+	User           string
+	Protocol       string
+	ContentType    string
+	ContentLength  int64
+	Host           string
 	ResponseStatus int
 	ResponseSize   int
+	TLSData        TLSData
 
 	RequestProcessingTime int64
 	LogProcessingTime     int64
+}
+
+type TLSData struct {
+	TLSVersion    uint16
+	TLSCipherUsed uint16
 }
 
 var FormatJSON = func(log LogItems) string {
@@ -60,20 +65,27 @@ func Logger_JSON(filename string, w_stdout bool) gin.HandlerFunc {
 		stop := time.Now().UnixNano()
 
 		log := LogItems{
-			ISOTime:       start,
-			UnixTime:      start.UnixNano(),
-			IP:            c.ClientIP(),
-			Method:        c.Request.Method,
-			User:          c.Request.URL.User.Username(),
-			Path:          c.Request.URL.EscapedPath(),
-			Query:         c.Request.URL.RawQuery,
-			Protocol:      c.Request.Proto,
-			ContentType:   c.ContentType(),
-			ContentLength: c.Request.ContentLength,
-			Host:          c.Request.Host,
-			//TLSVersion: c.Request.TLS.Version,
+			ISOTime:        start,
+			UnixTime:       start.UnixNano(),
+			IP:             c.ClientIP(),
+			Method:         c.Request.Method,
+			User:           c.Request.URL.User.Username(),
+			Path:           c.Request.URL.EscapedPath(),
+			Query:          c.Request.URL.RawQuery,
+			Protocol:       c.Request.Proto,
+			ContentType:    c.ContentType(),
+			ContentLength:  c.Request.ContentLength,
+			Host:           c.Request.Host,
 			ResponseStatus: c.Writer.Status(),
 			ResponseSize:   c.Writer.Size(),
+		}
+
+		if c.Request.TLS != nil {
+			// https://golang.org/pkg/crypto/tls/#pkg-constants
+			log.TLSData = TLSData{
+				TLSVersion:    c.Request.TLS.Version,
+				TLSCipherUsed: c.Request.TLS.CipherSuite,
+			}
 		}
 
 		log.RequestProcessingTime = stop - log.UnixTime
